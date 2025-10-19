d2449f28be04f7db43a9ebe66498067e59a830e1
filipcarevic <filip.carevic1008@gmail.com>
2025-10-19 15:33:51 +0200

    Drugi zadatak...koji sam kod napisa...UFF

--- zadatak2.c
+++ zadatak2.c
@@ -31,15 +31,101 @@ typedef struct person
 
 //kalup koda
 person* createNewPerson(const char*name, const char*surname, int birthYear); //fje. za stvaranje novog cvora bez da se uneseni string mjenja
-int addToStart(person**head, char*name, char*prezime, int birthYear);
-int printList(person* head);
+int addToStart(person**head, person*current);
+int printList(person*head);
 int addToEnd(person**head, person*current);
-int findElement(person* head, char*prezime);
-int deleteElement(person**head, char*prezime);
+int findElement(person*head, char*surname);
+int deleteElement(person**head, char*surname);
 /*
 person**head se koristi kad zelimo promjenit vrijesnodt headai njegovu adresu. A person* head kad zelimo promjenit
 samo kopiju tog heada, nije stalna promjena.
 */
+
+
+//mozak funkcija/coda
+person* createNewPerson(const char*name, const char*surname, int birthYear)
+{
+    person* current = (person*)malloc(sizeof(person));
+
+    strcpy(current->name, name); // -> sluzi kao pokazivac na strukturu
+    strcpy(current->surname, surname);
+    current->birthYear = birthYear;
+    current->next = NULL; // pokazivac pokazuje na novi element 
+    return current;
+}
+
+int addToEnd(person**head, person*current)
+{
+    if(*head==NULL)
+    {
+        *head = current; // ako je lista prazna, novi elemtn je na prvom(jedinom) mjestu
+        return EXIT_SUCCESS;
+    }
+
+    person *temp = *head; // sluzi da prodjemo kroz listu bez da diramo stvarni head 
+    while(temp->next != NULL) // Dok pokazivač next iz trenutnog čvora (temp) nije prazan (NULL), idi dalje
+    {
+        temp = temp->next;
+    }
+    temp->next = current; // zato sta petlja jos pokazuje na zadnji elemnt, u ovom slucaju je to 3. element
+    return EXIT_SUCCESS;
+}
+
+int printList(person*head)
+{
+    person *temp = head;
+    while(temp != NULL) // ne dok postoji iduci element, nego dok trenutni element nije nepostojec
+    {
+        printf("%s %s %d\n", temp->name, temp->surname, temp->birthYear);
+        temp = temp->next;
+    }
+    return EXIT_SUCCESS;
+}
+
+int deleteElement(person**head, char*surname)
+{
+    person *prev = NULL;
+    person *temp = *head;
+    while (temp != NULL && strcmp(temp->surname, surname) != 0)
+    {
+        prev = temp;
+        temp = temp->next;
+    }
+
+    // ako element nije pronađen
+    if (temp == NULL)
+    {
+        printf("taj element ne postoji");
+        return EXIT_FAILURE;
+    }
+
+    prev->next = temp->next; // doslovno skippa trenutni elent i na taj nacin lista ostane uredna 
+
+    free(temp); // brise preskoceni element
+    return EXIT_SUCCESS;
+}
+
+int findElement(person*head, char*surname)
+{
+    person *temp = head;
+    while (temp != NULL && strcmp(temp->surname, surname) != 0)
+    {
+        temp = temp->next;
+    }
+
+    // ako element nije pronađen
+    if (temp == NULL) printf("taj element ne postoji");
+    else printf("%s %s %d\n", temp->name, temp->surname, temp->birthYear);
+    return EXIT_SUCCESS;
+}
+
+int addToStart(person**head, person*current)
+{
+    current->next = *head; // novi cvor pokazuje na stari početak
+    *head = current; // head sad pokazuje na novi pocetk sta je novi cvor na prvom mjestu
+    return EXIT_SUCCESS;
+}
+
 
 //tjelo koda: odabir opcija i poziv funkcija
 int main()
@@ -57,48 +143,71 @@ int main()
     }
 
     fclose(file);
-    printList(head);
-}
 
-//mozak funkcija/coda
-// stvaranje nove osobe
-person* createNewPerson(const char*name, const char*surname, int birthYear)
-{
-    person* current = (person*)malloc(sizeof(person));
-
-    strcpy(current->name, name); // -> sluzi kao pokazivac na strukturu
-    strcpy(current->surname, surname);
-    current->birthYear = birthYear;
-    current->next = NULL; // pokazivac pokazuje na novi element 
-    return current;
-}
-
-// postavljanje nove osobena zadnje mjesto u listi
-int addToEnd(person**head, person*current)
-{
-    if(*head==NULL)
+    char input = 'T';
+    while(toupper(input) != 'F')
     {
-        *head = current; // ako je lista prazna, novi elemtn je na prvom(jedinom) mjestu
-        return EXIT_SUCCESS;
-    }
+        printf("T ili F: ");
+        scanf(" %c", &input);
 
-    person *temp = *head; // sluzi da prodjemo kroz listu bez da diramo stvarni head 
-    while(temp->next != NULL) // Dok pokazivač next iz trenutnog čvora (temp) nije prazan (NULL), idi dalje
-    {
-        temp = temp -> next;
-    }
-    temp->next = current; // zato sta petlja jos pokazuje na zadnji elemnt, u ovom slucaju je to 3. element
-    return EXIT_SUCCESS;
+        int c;
+        while ((c = getchar()) != '\n' && c != EOF);
 
-}
+        if(toupper(input) == 'F') break;
 
-int printList(person*head)
-{
-    person *temp = head;
-    while(temp != NULL) // ne dok postoji iduci element, nego dok trenutni element nije nepostojec
-    {
-        printf("%s %s %d\n", temp->name, temp->surname, temp->birthYear);
-        temp = temp -> next;
-    }
-    return EXIT_SUCCESS;
-}
+        char function;
+
+        printf("Odaberi izmedju:\n");
+        printf("A. dodaje novi element na početak liste\n");
+        printf("B. ispisuje listu\n");
+        printf("C. dodaje novi element na kraj liste\n");
+        printf("D. pronalazi element u listi (po prezimenu)\n");
+        printf("E. briše određeni element iz liste\n");
+        scanf(" %c", &function);
+    
+        person* current = NULL;
+        switch (function) 
+        {
+            case 'A':
+            {
+                printf("unesi ime prezima godinu rodenja tim redom sa razmakom izmedju svakog: ");
+                scanf(" %s %s %d", name, surname, &age);
+                current = createNewPerson(name, surname, age);
+                addToStart(&head, current);  
+                break;
+            }
+            case 'B':
+            {
+                printList(head);
+                break;
+            }
+            case 'C':
+            {
+                printf("unesi ime prezima godinu rodenja tim redom sa razmakom izmedju svakog: ");
+                scanf(" %s %s %d", name, surname, &age);
+                current = createNewPerson(name, surname, age);
+                addToEnd(&head, current);  
+                break;
+            }
+            case 'D':
+            {
+                printf("unesi prezima: ");
+                scanf(" %s", surname);
+                findElement(head, surname);
+                break;
+            }
+            case 'E':
+            {
+                printf("unesi prezima: ");
+                scanf(" %s", surname);
+                deleteElement(&head, surname);
+                break;
+            }
+            default:
+            {
+                printf("uensen je ne postojeca opcija: %c\n", function);
+            }
+        }    
+    }  
+    return 0;
+} 
